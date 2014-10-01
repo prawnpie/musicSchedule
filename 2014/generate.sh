6fd59b09417fe6b251467a9de22b93d54c6f2b4b
Volker Neumann <volker@yahoo-inc.com>
2014-10-01 11:17:37 -0700

    update generate script for saturday and sunday

--- 2014/generate.sh
+++ 2014/generate.sh
@@ -6,10 +6,10 @@ perl -ne 's#CONTENT_GOES_HERE#`cat tmp/friday.inc`#e;print' tmp/friday.html.tmp 
 
 ../sched.pl < scheduleSource/2014saturday.tab > tmp/saturday.inc
 cat ../template.html | sed 's/DAY_GOES_HERE/Saturday, October 4/g' | sed "s#BITLY_GOES_HERE#${bitly}#" > tmp/saturday.html.tmp
-perl -ne 's#CONTENT_GOES_HERE#`cat tmp/saturday.inc`#e;print' saturday.html.tmp > saturday.html
+perl -ne 's#CONTENT_GOES_HERE#`cat tmp/saturday.inc`#e;print' tmp/saturday.html.tmp > out/saturday.html
 
 ../sched.pl < scheduleSource/2014sunday.tab > tmp/sunday.inc
 cat ../template.html | sed 's/DAY_GOES_HERE/Sunday, October 5/g' | sed "s#BITLY_GOES_HERE#${bitly}#" > tmp/sunday.html.tmp
-perl -ne 's#CONTENT_GOES_HERE#`cat tmp/sunday.inc`#e;print' sunday.html.tmp > sunday.html
+perl -ne 's#CONTENT_GOES_HERE#`cat tmp/sunday.inc`#e;print' tmp/sunday.html.tmp > out/sunday.html
 
 rm *.tmp

37afdbfa9fd3bb6d8b0e8f4ca256e08f7b26a4c8
Volker Neumann <volker@dirtmine.com>
2015-09-27 21:58:13 -0700

    2015 script and schedule - needs some tweaking, artist names are long

---
+++ 2015/generate.sh
@@ -0,0 +1,31 @@
+bitly=http://bit.ly/2015hsb
+
+cp scheduleSource/index_page_qr.png out/
+
+mkdir tmp
+
+../sched.pl < scheduleSource/2015friday.tab > tmp/friday.inc
+cat ../template.html \
+| sed 's/DAY_GOES_HERE/Friday, October 2/g' \
+| sed "s#BITLY_GOES_HERE#${bitly}#" \
+| sed 's/QR_CODE_TOP/450/' | sed 's/QR_CODE_LEFT/790/' \
+> tmp/friday.html.tmp
+perl -ne 's#CONTENT_GOES_HERE#`cat tmp/friday.inc`#e;print' tmp/friday.html.tmp > out/friday.html 
+
+../sched.pl < scheduleSource/2015saturday.tab > tmp/saturday.inc
+cat ../template.html \
+| sed 's/DAY_GOES_HERE/Saturday, October 3/g' \
+| sed "s#BITLY_GOES_HERE#${bitly}#" \
+| sed 's/QR_CODE_TOP/450/' | sed 's/QR_CODE_LEFT/940/' \
+> tmp/saturday.html.tmp
+perl -ne 's#CONTENT_GOES_HERE#`cat tmp/saturday.inc`#e;print' tmp/saturday.html.tmp > out/saturday.html
+
+../sched.pl < scheduleSource/2015sunday.tab > tmp/sunday.inc
+cat ../template.html \
+| sed 's/DAY_GOES_HERE/Sunday, October 4/g' \
+| sed "s#BITLY_GOES_HERE#${bitly}#" \
+| sed 's/QR_CODE_TOP/450/' | sed 's/QR_CODE_LEFT/940/' \
+> tmp/sunday.html.tmp
+perl -ne 's#CONTENT_GOES_HERE#`cat tmp/sunday.inc`#e;print' tmp/sunday.html.tmp > out/sunday.html
+
+rm -rf tmp/
